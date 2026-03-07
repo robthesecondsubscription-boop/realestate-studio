d06c1e1808a30d7a771b68ff4d7996c185e525fd
MacBook pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-03-07 09:58:31 +0800

    fix wildcard route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/api/status/:id', (req, res) => {
 
 // Serve React build
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
