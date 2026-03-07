31628464b8ab5805be5ed4cdcf22ecea693c82c1
MacBook pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-03-07 10:08:28 +0800

    add CORS to express server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // API routes
